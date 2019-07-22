1a85ae8c21730b16402eaeecfee76343ca81a145
klguenth <klguenth@gmail.com>
2019-07-21 22:15:11 -0500

    updated Counter.js and added to App.js

---
+++ src/state/Counter.js
@@ -0,0 +1,17 @@
+class Counter extends React.Component {
+    state = {
+      count: 0
+    };
+    render() {
+        return (
+          <div>
+            <p>The current count: {this.state.count}</p>
+            <button
+              onClick={function() { console.log('clicked!') }}
+            >
+              Add 1
+            </button>
+          </div>
+        )
+      }
+  }
