cd7de8714bca36ae5090a30176601f6d649c9b2e
klguenth <klguenth@gmail.com>
2019-08-31 11:05:54 -0500

    added react router functionality

---
+++ src/paltrows-power-toes/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './paltrows-power-toes/App';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>, document.getElementById('root'));
